f4048ffbfbdbcb5a918184b1b2f6186b35f5c4e9
iRazzh <tanguygerardin60@hotmail.com>
2022-07-30 18:21:17 +0200

    advanced 85%

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.customer-logos'
+      });
+
     $(".customer-logos").slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
+        focusOnSelect: true,
         autoplaySpeed: 2000,
         arrows: false,
         dots: false,
         pauseOnHover: true,
+        centerMode: true,
+        asNavFor: '.slider-for',
         responsive: [
             {
                 breakpoint: 769,
@@ -23,3 +34,14 @@ $(document).ready(function () {
         ]
     });
 });
+
+/*----------------------------------------------------------------
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+*/
